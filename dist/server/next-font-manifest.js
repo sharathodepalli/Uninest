3aa9553c13b8c29392cef75443b02718abebad88
sharathodepalli <sharathodepalli@gmail.com>
2025-07-02 21:01:14 -0400

    Remove build artifacts from repo

--- dist/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/sharathchandra/Documents/Make it_2025/sample/UniNest/UniNest/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
